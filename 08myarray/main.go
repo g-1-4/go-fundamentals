a18d529115203ac94eb249f91269f459b3eaf7ff
gowtham <gowthamarepalli09@gmail.com>
2026-01-17 00:02:06 +0530

    Arrays, different type of declarations

---
+++ 08myarray/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Array in golang")
+
+	// Creating an array
+	var fruitList [4]string
+
+	// Assigning values to array
+	fruitList[0] = "Apple"
+	fruitList[1] = "Mango"
+	// fruitList[2] = "Banana"
+	fruitList[3] = "Grapes"
+
+	// Printing the array and its length
+	fmt.Println("The fruitList is:", fruitList)
+	fmt.Println("The length of fruitList is:", len(fruitList))
+
+	// Another way to declare and initialize an array
+	var vegList = [3]string{"Potato", "Tomato", "Cabbage"}
+
+	// Printing the vegList and its length
+	fmt.Println("The vegList is:", vegList)
+	fmt.Println("The length of vegList is:", len(vegList))
+}
